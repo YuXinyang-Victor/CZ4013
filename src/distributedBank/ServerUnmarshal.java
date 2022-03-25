2ebcd5269f7ba0f861b009098a81b2a608500bc6
FromTheFarNorth <XYU016@e.ntu.edu.sg>
2022-03-25 17:25:32 +0800

    Fixed bug regarding range of array to be read

--- src/distributedBank/ServerUnmarshal.java
+++ src/distributedBank/ServerUnmarshal.java
@@ -12,7 +12,7 @@ public class ServerUnmarshal {
 	}
 	
 	public static int getFieldLength(int curr_pos, byte[] message_in) {
-		byte[] field_length_byte = Arrays.copyOfRange(message_in, curr_pos, curr_pos+5);
+		byte[] field_length_byte = Arrays.copyOfRange(message_in, curr_pos, curr_pos+4);
 		ByteBuffer b = ByteBuffer.wrap(field_length_byte);
 		int field_length = b.getInt();
 		return field_length;
@@ -22,7 +22,7 @@ public class ServerUnmarshal {
 		//This method returns everything as string, which corresponds to client marshal methods
 		//as they were converted to strings before marshaling. The function receiving the string 
 		//should convert to the required data type accordingly. 
-		byte[] field_value_byte = Arrays.copyOfRange(message_in, curr_pos, curr_pos+field_length+1);
+		byte[] field_value_byte = Arrays.copyOfRange(message_in, curr_pos, curr_pos+field_length);
 		String field_value = new String(field_value_byte);
 		return field_value;
 	}
