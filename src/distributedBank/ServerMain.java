b5bdd844e5654ee809b5a6aa2b2f5f35fe6e8485
FromTheFarNorth <XYU016@e.ntu.edu.sg>
2022-03-25 17:35:25 +0800

    Update handler one; added notes for message format related update

--- src/distributedBank/ServerMain.java
+++ src/distributedBank/ServerMain.java
@@ -28,8 +28,13 @@ public class ServerMain {
 		
 		//Because of predetermined protocal, server knows that for opcode 1, it is a request for account creation
 		//The first field after uuid is going to be name, followed by password, currency, and amount
-		int curr_pos = 5;
+		int curr_pos = 4;
 		int name_length = ServerUnmarshal.getFieldLength(curr_pos, message_in);
-		
+		curr_pos += 4;
+		String name = ServerUnmarshal.getValue(curr_pos, name_length, message_in);\
+		curr_pos += name_length; 
+		String password = ServerUnmarshal.getValue(curr_pos, password_length, message_in); //password length is mutually agreed. Need to delete SHA1 afterwards and update accordingly
+		String Currency = ServerUnmarshal.getValue(curr_pos, field_length, message_in)
+		//Confused regarding whether we should have length for every field. Maybe it is simpler to have length. 
 	}
 }
