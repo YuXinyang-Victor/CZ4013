49fff199dfd132a69cda7e19bb133fb4385b630c
FromTheFarNorth <52026538+YuXinyang-Victor@users.noreply.github.com>
2022-04-03 11:59:04 +0800

    Removed debug lines

--- src/distributedBank/RemoveTrail.java
+++ src/distributedBank/RemoveTrail.java
@@ -7,15 +7,12 @@ public class RemoveTrail {
         while (bytes[i] == 0) {
             i--;
         }
-        System.out.println(i);
         byte[] copy = new byte[i + 1];
         int j = 0; 
         while (j <= i) {
-        	System.out.println(j);
         	copy[j] = bytes[j];
         	j+=1; 
         }
-        System.out.println(copy);
         return copy;
 	}
 }
